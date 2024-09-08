bda27207e3c22ae087130eba98a575bce5a1bd80
Harold Yesid Gonzalez Mancipe <xh4rz@hotmail.com>
2024-09-08 01:38:48 -0500

    227. Interfaces de PokeAPI

--- src/actions/pokemons/get-pokemons.ts
+++ src/actions/pokemons/get-pokemons.ts
@@ -1,13 +1,27 @@
 import { pokeApi } from '../../config/api/pokeApi';
-import { Pokemon } from '../../domain/entities/pokemon';
+import type { Pokemon } from '../../domain/entities/pokemon';
+import type {
+	PokeAPIPaginatedResponse,
+	PokeAPIPokemon
+} from '../../infrastructure/interfaces/pokepi.interfaces';
 
-export const getPokemons = async (): Promise<Pokemon[]> => {
+export const getPokemons = async (
+	page: number,
+	limit = 20
+): Promise<Pokemon[]> => {
 	try {
-		const url = '/pokemon';
+		const url = `/pokemon?offset=${page * 10}&limit=${limit}`;
 
-		const { data } = await pokeApi.get(url);
+		const { data } = await pokeApi.get<PokeAPIPaginatedResponse>(url);
 
-		console.log(data);
+		const pokemonPrommises = data.results.map(info => {
+			return pokeApi.get<PokeAPIPokemon>(info.url);
+		});
+
+		const pokeApiPokemons = await Promise.all(pokemonPrommises);
+
+		console.log(pokeApiPokemons);
+
 		return [];
 	} catch (error) {
 		throw new Error('Error getting pokemons');
