f60ac8768e81e973cbf7908ab1f5ebd926631f13
Harold Yesid Gonzalez Mancipe <xh4rz@hotmail.com>
2024-09-07 21:02:38 -0500

    223. React Native Paper

--- src/presentation/screens/home/HomeScreen.tsx
+++ src/presentation/screens/home/HomeScreen.tsx
@@ -1,9 +1,13 @@
 import { Text, View } from 'react-native';
+import { Button } from 'react-native-paper';
 
 export const HomeScreen = () => {
 	return (
 		<View>
 			<Text>HomeScreen</Text>
+			<Button mode="contained" onPress={() => console.log('Pressed')}>
+				Press me
+			</Button>
 		</View>
 	);
 };
