116d17122c571ababb8f82169083b3e9c80879d8
Harold Yesid Gonzalez Mancipe <xh4rz@hotmail.com>
2024-09-08 01:20:42 -0500

    226. TanStack Query - Peticiones y caché

--- src/presentation/screens/home/HomeScreen.tsx
+++ src/presentation/screens/home/HomeScreen.tsx
@@ -1,15 +1,26 @@
 import { View } from 'react-native';
-import { Button, Text } from 'react-native-paper';
+import { ActivityIndicator, Button, Text } from 'react-native-paper';
 import { getPokemons } from '../../../actions/pokemons';
+import { useQuery } from '@tanstack/react-query';
 
 export const HomeScreen = () => {
-	getPokemons();
+	const { isLoading, data } = useQuery({
+		queryKey: ['pokemons'],
+		queryFn: () => getPokemons(),
+		staleTime: 1000 * 60 * 60 // 60 minutes
+	});
+
 	return (
 		<View>
 			<Text variant="displaySmall">HomeScreen</Text>
-			<Button mode="contained" onPress={() => console.log('Pressed')}>
-				Press me
-			</Button>
+
+			{isLoading ? (
+				<ActivityIndicator />
+			) : (
+				<Button mode="contained" onPress={() => console.log('Pressed')}>
+					Press me
+				</Button>
+			)}
 		</View>
 	);
 };
