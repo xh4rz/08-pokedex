f70f558372bfbee665450f7d221ba84c87ec258f
Harold Yesid Gonzalez Mancipe <xh4rz@hotmail.com>
2024-09-07 23:31:01 -0500

    225. Interfaces, entidades y petición http

--- src/presentation/screens/home/HomeScreen.tsx
+++ src/presentation/screens/home/HomeScreen.tsx
@@ -1,7 +1,9 @@
 import { View } from 'react-native';
 import { Button, Text } from 'react-native-paper';
+import { getPokemons } from '../../../actions/pokemons';
 
 export const HomeScreen = () => {
+	getPokemons();
 	return (
 		<View>
 			<Text variant="displaySmall">HomeScreen</Text>
