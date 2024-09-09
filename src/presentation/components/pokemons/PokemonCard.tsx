cb01f19b6423ec8a01a51f1b27aef8b072345112
Harold Yesid Gonzalez Mancipe <xh4rz@hotmail.com>
2024-09-09 01:47:52 -0500

    231. FadeIn Image y useAnimation

--- src/presentation/components/pokemons/PokemonCard.tsx
+++ src/presentation/components/pokemons/PokemonCard.tsx
@@ -1,6 +1,7 @@
 import { Image, StyleSheet, View } from 'react-native';
 import { Pokemon } from '../../../domain/entities/pokemon';
 import { Card, Text } from 'react-native-paper';
+import { FadeInImage } from '../ui/FadeInImage';
 
 interface Props {
 	pokemon: Pokemon;
@@ -20,7 +21,7 @@ export const PokemonCard = ({ pokemon }: Props) => {
 				/>
 			</View>
 
-			<Image source={{ uri: pokemon.avatar }} style={styles.pokemonImage} />
+			<FadeInImage uri={pokemon.avatar} style={styles.pokemonImage} />
 
 			<Text style={[styles.name, { marginTop: 35 }]}>{pokemon.types[0]}</Text>
 		</Card>
