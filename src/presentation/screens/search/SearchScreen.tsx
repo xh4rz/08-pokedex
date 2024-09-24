0bb7dd4b1b28de72f0a04086bc245e5a5fff8fef
Harold Yesid Gonzalez Mancipe <xh4rz@hotmail.com>
2024-09-24 05:12:14 -0500

    245. Filtrar listado por id y nombre

--- src/presentation/screens/search/SearchScreen.tsx
+++ src/presentation/screens/search/SearchScreen.tsx
@@ -1,21 +1,38 @@
 import { FlatList, View } from 'react-native';
 import { globalTheme } from '../../../config/theme/global-theme';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
-import { ActivityIndicator, TextInput } from 'react-native-paper';
+import { ActivityIndicator, Text, TextInput } from 'react-native-paper';
 import { Pokemon } from '../../../domain/entities/pokemon';
 import { PokemonCard } from '../../components/pokemons/PokemonCard';
 import { useQuery } from '@tanstack/react-query';
 import { getPokemonNamesWithId } from '../../../actions/pokemons';
+import { useMemo, useState } from 'react';
 
 export const SearchScreen = () => {
 	const { top } = useSafeAreaInsets();
+
+	const [term, setTerm] = useState('');
 
 	const { isLoading, data: pokemonNameList = [] } = useQuery({
 		queryKey: ['pokemons', 'all'],
 		queryFn: () => getPokemonNamesWithId()
 	});
 
-	console.log(pokemonNameList);
+	const pokemonNameIdList = useMemo(() => {
+		// es un numero
+		if (!isNaN(Number(term))) {
+			const pokemon = pokemonNameList.find(poke => poke.id === Number(term));
+			return pokemon ? [pokemon] : [];
+		}
+
+		if (term.length === 0) return [];
+
+		if (term.length < 3) return [];
+
+		return pokemonNameList.filter(pokemon =>
+			pokemon.name.includes(term.toLocaleLowerCase())
+		);
+	}, [term]);
 
 	return (
 		<View style={[globalTheme.globalMargin, { paddingTop: top }]}>
@@ -24,11 +41,13 @@ export const SearchScreen = () => {
 				mode="flat"
 				autoFocus
 				autoCorrect={false}
-				onChangeText={value => console.log(value)}
-				value={''}
+				onChangeText={setTerm}
+				value={term}
 			/>
 
 			<ActivityIndicator style={{ paddingTop: 20 }} />
+
+			<Text>{JSON.stringify(pokemonNameIdList, null, 2)}</Text>
 
 			<FlatList
 				data={[] as Pokemon[]}
